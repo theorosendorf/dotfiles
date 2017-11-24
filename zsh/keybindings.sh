c85268d66e1597513a8e3c429c6a71d02b1e4899
Theo Rosendorf <theo@type.fans>
2017-11-24 02:06:25 -0500

    Added and edited tmux config from WOPR

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -17,14 +17,14 @@
 			then
 				BUFFER="git add -A; git commit -v && git push"
 		fi
-				
+
 		zle accept-line
 	}
 	zle -N git_prepare
 	bindkey "^g" git_prepare
 
 # home
-	function goto_home() { 
+	function goto_home() {
 		BUFFER="cd ~/"$BUFFER
 		zle end-of-line
 		zle accept-line
